4933301abf72ade40302a2b05a05eb1ac73eae54
siwangqishiq <525647740@qq.com>
2013-03-26 13:55:42 +0800

    音量

--- paopao/src/com/xinlan/bubble/component/BgmPlayer.java
+++ paopao/src/com/xinlan/bubble/component/BgmPlayer.java
@@ -22,6 +22,7 @@ public class BgmPlayer {
 		} else if (rand == 2){
 			mediaPlayer = MediaPlayer.create(context, R.raw.bg3);
 		}
+		mediaPlayer.setVolume(0.5f, 0.5f);
 		mediaPlayer.setLooping(true);// 循环播放
 	}
 
